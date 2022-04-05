6f190a46f12a3aca7b9603429ff558fdf06efb46
kasev <vojtech.kase@gmail.com>
2022-04-05 17:06:42 +0200

    started working one article revisions

--- create_ascnet_venv.sh
+++ create_ascnet_venv.sh
@@ -6,12 +6,9 @@ python3 -m venv $VENVNAME
 source $VENVNAME/bin/activate
 pip install --upgrade pip
 
-pip install ipython
-pip install jupyter
+test -f requirements.txt && pip install -r requirements.txt
 
 python -m ipykernel install --user --name=$VENVNAME
-
-test -f requirements.txt && pip install -r requirements.txt
 
 deactivate
 echo "build $VENVNAME"
